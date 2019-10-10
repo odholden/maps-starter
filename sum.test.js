e3a99713754811225ac6057f3b2a07fba9107a41
Shea.Murphy <shea@nology.io>
2019-10-10 08:29:52 +0100

    All config, setup and examples completed

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,30 @@
-const sum = require("./sum");
+import { sum, Person } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+describe("These are my class tests", () => {
+  let testPerson;
+  beforeEach(() => {
+    testPerson = new Person("barry", 19);
+  });
+
+  test("Sets name", () => {
+    expect(testPerson.name).toEqual("barry");
+  });
+
+  test("Sets age", () => {
+    expect(testPerson.age).toEqual(19);
+  });
+
+  test("Gets name string", () => {
+    expect(testPerson.getNameString()).toEqual("Hello my name is barry");
+  });
+
+  test("Sets new age", () => {
+    expect(testPerson.age).toEqual(19);
+    testPerson.age = 20;
+    expect(testPerson.age).toEqual(20);
+  });
 });
--- sum.ts
+++ sum.ts
@@ -1,3 +1,25 @@
 export const sum = (a, b) => {
   return a + b;
 };
+
+export class Person {
+  name: string;
+  _age: number;
+
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
+  }
+
+  getNameString() {
+    return `Hello my name is ${this.name}`;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(age: number) {
+    this._age = age;
+  }
+}
