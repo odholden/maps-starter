c97a02edb0d19703915b5b225e290ab03f239f40
Ollie Holden <odholden@gmail.com>
2019-10-15 08:14:45 +0100

    simple maps starter including typescript build for google maps

--- src/js/maps/maps.ts
+++ src/js/maps/maps.ts
@@ -1,4 +1,5 @@
 import {} from 'googlemaps';
+import { styles } from './maps-data';
 
 export interface ILocation {
   lat: number,
@@ -8,7 +9,9 @@ export interface ILocation {
 export const initMap = (coordinates: ILocation): google.maps.Map => {
   var map: google.maps.Map = new google.maps.Map(
     document.getElementById('map'), 
-    {zoom: 4, center: coordinates}
+    { zoom: 4, 
+      center: coordinates
+    }
   );
   var marker: google.maps.Marker = new google.maps.Marker({position: coordinates, map: map});
   return map;
